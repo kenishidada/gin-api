fd0e8db153a25dddcc24359f7a79080767c3c150
KenIshida <kenishida95@gmail.com>
2024-05-18 00:50:01 +0900

    [add] add auth

---
+++ controllers/auth_controller.go
@@ -0,0 +1,36 @@
+package controllers
+
+import (
+	"gin-api/dto"
+	"gin-api/services"
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+)
+
+type IAuthController interface {
+	SignUp(ctx *gin.Context)
+}
+
+type AuthController struct {
+	service services.IAuthService
+}
+
+func NewAuthController(service services.IAuthService) IAuthController {
+	return &AuthController{service: service}
+}
+
+func (c *AuthController) SignUp(ctx *gin.Context) {
+	var input dto.SignUpInput
+	if err := ctx.ShouldBindJSON(&input); err != nil {
+		ctx.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+		return
+	}
+
+	if err := c.service.SignUp(input.Email, input.Password); err != nil {
+		ctx.JSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
+		return
+	}
+
+	ctx.JSON(http.StatusOK, gin.H{"data": "success"})
+}
--- main.go
+++ main.go
@@ -14,22 +14,23 @@ func main() {
 	infra.Initialize()
 	db := infra.SetupDB()
 
-	// items := []models.Item{
-	// 	{ID: 1, Name: "item1", Price: 100, Description: "This is item1", SoldOut: false},
-	// 	{ID: 2, Name: "item2", Price: 200, Description: "This is item2", SoldOut: true},
-	// 	{ID: 3, Name: "item3", Price: 300, Description: "This is item3", SoldOut: false},
-	// }
-
-	// itemRepository := repositories.NewItemMemoryRepository(items)
 	itemRepository := repositories.NewItemRepository(db)
 	itemService := services.NewItemService(itemRepository)
 	itemController := controllers.NewItemController(itemService)
 
+	authRepository := repositories.NewAuthRepository(db)
+	authService := services.NewAuthService(authRepository)
+	authController := controllers.NewAuthController(authService)
+
 	r := gin.Default()
-	r.GET("/items", itemController.FindAll)
-	r.GET("/items/:id", itemController.FindById)
-	r.POST("items", itemController.Create)
-	r.PUT("items/:id", itemController.Update)
-	r.DELETE("items/:id", itemController.Delete)
+	itemRouter := r.Group("/items")
+	authRouter := r.Group("/auth")
+	itemRouter.GET("", itemController.FindAll)
+	itemRouter.GET("/:id", itemController.FindById)
+	itemRouter.POST("", itemController.Create)
+	itemRouter.PUT("/:id", itemController.Update)
+	itemRouter.DELETE("/:id", itemController.Delete)
+
+	authRouter.POST("/resister", authController.SignUp)
 	r.Run() // 0.0.0.0:8080 でサーバーを立てます。
 }
